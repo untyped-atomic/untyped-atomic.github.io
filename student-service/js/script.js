ae62c2a592f824fdc5774ff32bc1411e7a5ef7e2
untyped-atomic <78023492+untyped-atomic@users.noreply.github.com>
2021-07-29 21:07:02 +0300

    Update script.js

--- student-service/js/script.js
+++ student-service/js/script.js
@@ -13,4 +13,12 @@ $('.qna__set__question').click(function(){
    $(this).hide();
    $('.text *:not(.text-unfold-btn-wrap)').show();
   });
+  
+  $(window).on('load resize', function() {
+   if( $(window).width() < 1200)  {
+      $('.pinned-ok').insertAfter('.jmb-mbl');     
+   	} else {
+   	$('.pinned-ok').insertAfter('.get-price-top');	
+   	}
+   });
 });
